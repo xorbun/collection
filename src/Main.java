fe18fcc7d516d22e5d21227b4cbae1583cd39a0d
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-12-05 17:17:32 +0100

    xorbun
    completed hashset and Arraylist function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,6 +6,8 @@ public class Main
 {
     public static void main(String[] args)
     {
+
+        //ESERCIZIO 1
       Scanner input=new Scanner(System.in);
       Scanner input2=new Scanner(System.in);
       int lunghezza=input.nextInt();
@@ -22,9 +21,34 @@ public class Main
              duplicate.add(parola);
          }
       }
-      System.out.println(words);
-      System.out.println(words.size());
-      System.out.println(duplicate);
+      System.out.println("parole distinte"+words);
+      System.out.println("totale parole distinte"+words.size());
+      System.out.println("totale parole duplici"+duplicate);
+            //ESERCIZIO 2
+       int valore=input.nextInt();
+       ArrayList<Integer>risultato=new ArrayList<>();
+       ArrayList<Integer>reversearray=new ArrayList<>();
+       risultato=randomnum(valore);
+       System.out.println(risultato);
+        reversearray=reverseoption(risultato);
+        System.out.println(reversearray);
 
     }
+    public static ArrayList randomnum(int n)
+    {
+        Random rndm=new Random();
+        ArrayList <Integer> numeri=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            numeri.add(rndm.nextInt(1,100));
+        }
+        return numeri;
+    }
+    public static ArrayList reverseoption(ArrayList vect)
+    {
+        Collections.reverse(vect);
+        return vect;
+    }
+
+
 }
